97875bc7f6d198e29ca878c155cdd9f4980ece97
naekang <66901361+naekang@users.noreply.github.com>
2025-02-17 06:54:49 +0000

    Deploying to gh-pages from @ naekang/naekang.github.io@3180c28153492236a55e09c228589f63ae3d8306 🚀

---
+++ _app/immutable/chunks/education.CDFG1fyW.js
@@ -0,0 +1,1 @@
+import{A as o}from"./assets.3Fv6v9MM.js";const t="Education",e=[{degree:"컴퓨터과학부 학사",description:"",location:"Seoul, Korea",logo:o.UosLogo,name:"",organization:"University of Seoul",period:{from:new Date(2017,2,2),to:new Date(2023,7,22)},shortDescription:"",slug:"uos-bachelor",subjects:[]}],i={title:t,items:e};export{i as E};
