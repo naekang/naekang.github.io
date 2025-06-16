173d2e684a56f6fff6884f7d77446afdf0d74fc4
JinhoKim <ts1321@tidesquare.com>
2025-06-16 09:15:39 +0900

    feat: Add kubecost blog

--- src/lib/data/blogs.ts
+++ src/lib/data/blogs.ts
@@ -2,6 +2,7 @@ import Assets from './assets';
 import { getSkills } from './skills';
 import type { Blog } from './types';
 import karpenterMd from './md/karpenter.md?raw';
+import kubecostMd from './md/kubecost.md?raw';
 const items: Array<Blog> = [
 	{
 		slug: 'karpenter',
@@ -12,8 +13,24 @@ const items: Array<Blog> = [
 		logo: Assets.AWS,
 		name: 'Karpenter 전환',
 		period: {
-			from: new Date(2023, 2, 2),
-			to: new Date(2024, 2, 1)
+			from: new Date(2025, 6, 15),
+			to: new Date(2025, 6, 15)
+		},
+		skills: getSkills('terraform', 'aws'),
+		type: 'Infra',
+		screenshots: []
+	},
+	{
+		slug: 'kubecost',
+		color: '#ff3e00',
+		description: kubecostMd,
+		shortDescription: 'Kubecost 설치하기',
+		links: [],
+		logo: Assets.AWS,
+		name: 'Kubecost 설치하기',
+		period: {
+			from: new Date(2025, 6, 16),
+			to: new Date(2025, 6, 16)
 		},
 		skills: getSkills('terraform', 'aws'),
 		type: 'Infra',
